c4c85b1d332b0143008ce9acc478b3982502b415
levan1199 <vipchicken28@gmail.com>
2021-07-20 17:02:40 +0700

    adjust

--- tutimServerSide/tutimServer/config.js
+++ tutimServerSide/tutimServer/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
+    'secretKey': '',
     'mongoUrl': 'mongodb://localhost:27017/tutim',
     'facebook':{
-        clientId:'339580844299731',
-        clientSecret:'fce9e373dd14d86ad8578bb33fc8c913'
+        clientId:'',
+        clientSecret:''
     }
 }
